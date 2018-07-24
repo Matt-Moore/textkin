231d08aa1b7f2c6e0d389977a308a59a4794e0a1
Matthew Moore <matthew@ocicat.media>
2018-07-24 10:11:13 -0400

    Basic attack, and enemy death funcgtions added. Planning overhaul of item system.

--- game.py
+++ game.py
@@ -1,20 +1,37 @@
+import random
 import xml.etree.ElementTree as ET
 
 class World:
 	def __init__(self,path='data/'):
-		self.data = {'items':ET.parse(path + 'items.xml').getroot()}
+		self.data = {'items':ET.parse(path + 'items.xml').getroot(),'monsters':ET.parse(path + 'monsters.xml').getroot()}
 		
-		self.items = {'weapons':{},'armour':{}}
+		self.items = {}
 		self.rooms = {}
+		self.monsters = {}
 		
 		# CREATE items
 		# CREATE items : weapons
 		for item in self.data['items'].find('weapons'):
-			self.items['weapons'][item.get('id')] = Weapon(item)
-			
+			self.items[item.get('id')] = Weapon(item)
+		# CREATE items : armour
 		for item in self.data['items'].find('armour'):
-			self.items['armour'][item.get('id')] = Armour(item)
+			self.items[item.get('id')] = Armour(item)
 						
+		# CREATE monsters
+		for monster in self.data['monsters']:
+			self.monsters[monster.get('id')] = Monster(monster)
+			
+		
+		# SHUFFLE items
+		self.deckItems = list(self.items)
+		random.shuffle(self.deckItems)
+		
+	def drawDeck(self,numDraws):
+		for i in range(numDraws):
+			player.invPickup(self.items[self.deckItems[0]])
+			self.deckItems.remove(self.deckItems[0])
+			
+		
 		
 class Item:
 	def __init__(self,dataInit):
@@ -22,7 +39,6 @@ class Item:
 		
 		self.id = dataInit.get('id')
 		self.name = dataInit.find('name').text
-		
 		self.weight = int(stats.find('weight').text)
 		
 		self.attack = 0
@@ -50,16 +66,95 @@ class Armour(Item):
 		super().__init__(dataInit)
 
 
-class Player:
+		
+class Character:
+	def actAttack(self,enemy):
+		if (type(self).__name__ == "Player"):
+			pObj = self
+			eObj = enemy
+			
+			order = 'player'
+			order = 'enemy'
+		else:
+			pObj = enemy
+			eObj = self
+			
+			order = 'enemy'
+			
+		pAtk = pObj.level + self.stats['attack']
+		pDef = pObj.level + self.stats['ac']
+		eAtk = eObj.stats['attack']
+		eDef = eObj.stats['attack']
+		
+		pAtkDif = pAtk - eDef
+		eAtkDif = eAtk - pDef
+		
+		if (order == 'player'):
+			if (pAtkDif > 0):
+				print("  Your attack deals {} damage to the {}.".format(pAtkDif,eObj.name))
+				eObj.stats['hp'][0] -= pAtkDif
+			else:
+				print("  Your attack misses the {}.".format(eObj.name))
+				
+			if (eAtkDif > 0):
+				print("  The {} attacks back, dealing {} damage.".format(eObj.name,eAtkDif))
+				pObj.stats['hp'][0] -= eAtkDif
+			else:
+				print("  The {} attacks back, but misses.".format(eObj.name))
+		
+		else:
+			if (eAtkDif > 0):
+				print("  The {} attacks you, dealing {} damage.".format(eObj.name,eAtkDif))
+				pObj.stats['hp'][0] -= eAtkDif
+			else:
+				print("  The {} attacks you, but misses.".format(eObj.name))
+		
+			if (pAtkDif > 0):
+				print("  You attack back, dealing {} damage to the {}.".format(pAtkDif,eObj.name))
+				eObj.stats['hp'][0] -= pAtkDif
+			else:
+				print("  You attack back, but miss the {}.".format(eObj.name))
+				
+		
+		
+		
+		
+		
+		
+		print (pObj.stats['hp'][0],eObj.stats['hp'][0])
+			
+		if (pObj.stats['hp'][0] <= 0 and eObj.stats['hp'][0] <= 0):
+			if (order == 'player'):
+				pObj.stats['hp'][0] = 1
+				print("  You manage to slay the {}, but you are left on the brink of death.".format(eObj.name))
+			else:
+				print("  Your last act has been to kill the {}.".format(eObj.name))
+				print("=== END GAME ===")
+		elif (pObj.stats['hp'][0] <= 0):
+			print("  The {} has slain you.".format(eObj.name))
+			print("=== END GAME ===")
+		
+		elif (eObj.stats['hp'][0] <= 0):
+			print("  You have defeated the {}.".format(eObj.name))
+			eObj.actDie()
+		
+		
+		
+		
+		
+		
+		
+
+class Player(Character):
 	def __init__(self):
 		self.name = "Sir Cheese"
 		self.level = 1
 		
 		self.inventory = []
-		self.equipped = [] #{'weapon':[],'head':None,'torso':None,'legs':None,'misc':[]}
-		self.weight = [0,7]
+		self.equipped = []
+		self.slot = {'head':None,'torso':None,'legs':None}
 		
-		self.stats = {'attack':0,'ac':0,'charisma':0,'weight':[0,7]}
+		self.stats = {'attack':0,'ac':0,'charisma':0,'weight':[0,7],'hp':[10,10]}
 		
 	def invPickup(self,item):
 		self.inventory.append(item)
@@ -78,14 +173,11 @@ class Player:
 		if (len(item)):
 			item = item[0]
 			if (self.stats['weight'][0] + item.weight <= self.stats['weight'][1]):
-				'''
-				if (type(item).__name__ == 'Weapon'):
-					self.equipped['weapon'].append(item)
-					self.equipped['weapon'].sort(key=lambda item:item.name)
-				elif (type(item).__name__ == 'Armour'):
-					self.equipped[item.slot] = item
-				'''
-			
+				if (not item.slot is None):
+					if (not self.slot[item.slot] is None):
+						self.invUnequip(self.slot[item.slot].name)
+					self.slot[item.slot] = item
+
 				self.equipped.append(item)
 				self.equipped.sort(key=lambda item:item.name)
 				self.inventory.remove(item)
@@ -116,6 +208,64 @@ class Player:
 			print("  You remove the {}.\n".format(item.name))
 		else:
 			print("  You don't have {} equipped.\n".format(itemName))
+			
+
+			
+			
+			
+class Monster(Character):
+	def __init__(self,dataInit):
+		stats = dataInit.find('stats')
+		
+		self.id = dataInit.get('id')
+		self.name = dataInit.find('name').text
+
+
+		self.stats = {'attack':int(stats.find('attack').text),'ac':int(stats.find('attack').text),'charisma':int(stats.find('attack').text),'hp':[int(stats.find('hp').text),int(stats.find('hp').text)],'treasure':int(stats.find('treasure').text)}
+
+		
+
+
+		
+		self.level = 0
+		self.aggro = False
+		
+		stat = stats.find('level')
+		if (not stat is None):
+			self.level = int(stat.text)
+		
+		stat = stats.find('aggro')
+		if (not stat is None and stat == 'true'):
+			self.aggro = True
+			
+	def actDie(self):
+		
+		if (self.stats['treasure']):
+			if (self.stats['treasure'] > 1):
+				print("  Searching the corpse of the {}, you find {} treasures.".format(self.name,self.stats['treasure']))
+			else:
+				print("  Searching the corpse of the {}, you find 1 treasure.".format(self.name))
+			
+			world.drawDeck(self.stats['treasure'])
+			
+		else:
+			print("  Searching the corpse of the {}, you find no treasures.".format(self.name))
+			
+		
+		
+		
+			
+		'''	
+			<attack>3</attack>
+			<treasure>1</treasure>
+			<level>1</level>
+			
+		'''	
+			
+			
+			
+			
+			
 '''
 
 				
@@ -307,33 +457,37 @@ world = World()
 player = Player()
 
 
-print("Inventory")
-player.invLook()
-
 print("--Picking up Items\n")
-player.invPickup(world.items['weapons']['w001'])
+player.invPickup(world.items['w001'])
+'''
 player.invPickup(world.items['weapons']['w002'])
 player.invPickup(world.items['armour']['ah001'])
-
+player.invPickup(world.items['armour']['ah002'])
+'''
 print("Inventory")
 player.invLook()
 
 print("== {}\n".format(player.stats))
 
-print("Equip big Wooden SwoRD")
-player.invEquip("big Wooden SwoRD")
+print("Equip Wooden SwoRD")
+player.invEquip("Wooden SwoRD")
 
-print("== {}\n".format(player.stats))
+print("===")
+print("World Deck: {}".format(world.deckItems))
+print("Inventory: {}".format(player.inventory))
+print("===\n")
 
-print("Equip HAT")
-player.invEquip("hat")
+print("Attack Bunny")
+player.actAttack(world.monsters["m001"])
 
-print("== {}\n".format(player.stats))
+print("Attack Bunny")
+player.actAttack(world.monsters["m001"])
 
-print("Remove HAT")
-player.invUnequip("hat")
+print("\n\n===")
+print("World Deck: {}".format(world.deckItems))
+print("Inventory: {}".format(player.inventory))
+print("===\n")
 
-print("== {}\n".format(player.stats))
 
 
 
